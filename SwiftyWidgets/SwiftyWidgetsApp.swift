b6907466f01307a56b0553fe323e91dd29bba0f3
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-01-20 14:31:34 +0200

    Initial Commit

---
+++ SwiftyWidgets/SwiftyWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWidgetsApp.swift
+//  SwiftyWidgets
+//
+//  Created by Pat on 2023/01/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
